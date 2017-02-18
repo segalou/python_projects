6ef949b99ebc7b0b17597eebf65b114d22bd1eb0
segalou <sega_lou@hotmail.com>
2017-02-18 22:32:40 +0800

    first commit

---
+++ three_doors/three_doors.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[7]:
+
+import random
+
+stay_win, switch_win = 0, 0
+sim_rounds = 10000
+
+for i in range(sim_rounds):
+    doors = {
+        1: "sheep",
+        2: "sheep",
+        3: "sheep"
+    } 
+
+    doors[random.randint(1,3)] = 'car'
+    choice = random.randint(1,3)
+    while True:
+        i = random.randint(1,3)
+        if i <> choice and doors[i] == "sheep":
+            doors.pop(i)
+            break
+    
+    if doors[choice] == 'car':
+        stay_win += 1.0
+    else:
+        switch_win += 1.0
+
+print stay_win/sim_rounds
+print switch_win/sim_rounds
+
+
+# In[127]:
+
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+print random.randint(1,3)
+
+
+# In[ ]:
+
+
+
