4a5f0d8faba278a5b21dc27266d4abece623f387
Sega <sega_lou@hotmail.com>
2017-03-10 19:40:17 +0800

    minor update

--- crossin_weekly/parlindrome.py
+++ crossin_weekly/parlindrome.py
@@ -4,7 +4,7 @@ Created on Fri Mar 10 17:30:39 2017
 
 @author: sega.lou
 """
-
+"""
 def palindrome(n):
     result = []
     for i in range(1,10):
@@ -22,3 +22,19 @@ def palindrome(n):
 
 n = 52
 palindrome(n)
+"""
+
+def parlindrome(n):
+    result = []
+    for i in range(10000,1000000):
+        i_sum=0
+        i_str = str(i)
+        if i_str == i_str[::-1]:
+            for j in i_str:
+                i_sum+=int(j)
+            if i_sum==n:
+                result.append(i)
+    for number in result:
+        print number       
+            
+parlindrome(52)
